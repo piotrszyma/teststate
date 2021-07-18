dad63fecbf706d38c5b3c7692be1fd93829f3a78
Piotr Szyma <thompson2908@gmail.com>
2021-07-18 19:47:32 +0200

    feat: types of beforeEach from @types/jest

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
 type TestStateFactory<T> = () => T;
 
+interface DoneCallback {
+    (...args: any[]): any
+    fail(error?: string | { message: string }): any;
+}
+
+interface ProvidesCallback {
+    (cb: DoneCallback): any;
+}
+
+// Extracted from @types/jest.
+// TODO: Check compatibility with other test runners.
+interface Lifecycle {
+    (fn: ProvidesCallback): any;
+}
+
 export function testState<T extends { [key in string]: any }>(
   factory: TestStateFactory<T>,
-  beforeEach: (cb: () => void) => void
+  beforeEach: Lifecycle,
 ): T {
   const state =  factory();
 
