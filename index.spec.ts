befd04b6a7635e4fd6632949eb5d55e6d1a5d03d
Piotr Szyma <thompson2908@gmail.com>
2022-05-04 18:42:50 +0200

    feat: add tests

--- index.spec.ts
+++ index.spec.ts
@@ -1,4 +1,4 @@
-import { testState } from ".";
+import { testState, resetState } from ".";
 
 describe("testState", () => {
   const state = testState(() => {
@@ -15,7 +15,6 @@ describe("testState", () => {
     expect(state.a).toEqual(2);
   });
 
-
   it("recreates state after each test case 2", () => {
     expect(state.a).toEqual(1);
 
@@ -26,5 +25,67 @@ describe("testState", () => {
 
   it("has clean state initially", () => {
     expect(state.a).toEqual(1);
+  });
+});
+
+describe("resetState", () => {
+  it("sets state id to identify state", () => {
+    const state = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    resetState(state);
+
+    expect((state as any)["___testStateStateId"]).toBeDefined();
+  });
+
+  it("sets state id as non enumerable", () => {
+    const state = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    resetState(state);
+
+    expect(Object.keys(state)).toEqual(["a", "b", "c"]);
+  });
+
+  it("clears state on subsequent resetState calls", () => {
+    const state = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    resetState(state);
+
+    state.a = 2;
+
+    resetState(state);
+
+    expect(state.a).toEqual(1);
+  });
+
+  it("can be used for multiple states", () => {
+    const stateA = { a: 1 };
+    const stateB = { b: 2 };
+
+    resetState(stateA);
+
+    stateA.a = 2;
+
+    resetState(stateA);
+
+    resetState(stateB);
+
+    stateB.b = 1;
+
+    resetState(stateB);
+
+    expect(stateA).toStrictEqual({ a: 1 });
+    expect(stateB).toStrictEqual({ b: 2 });
   });
 });
--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 type TestStateFactory<T> = () => T;
 
 interface DoneCallback {
-    (...args: any[]): any
-    fail(error?: string | { message: string }): any;
+  (...args: any[]): any;
+  fail(error?: string | { message: string }): any;
 }
 
 interface ProvidesCallback {
-    (cb: DoneCallback): any;
+  (cb: DoneCallback): any;
 }
 
 // Extracted from @types/jest.
 // TODO: Check compatibility with other test runners.
 interface Lifecycle {
-    (fn: ProvidesCallback): any;
+  (fn: ProvidesCallback): any;
 }
 
 export function testState<T>(
@@ -29,4 +29,36 @@ export function testState<T>(
   });
 
   return state;
+}
+
+let stateId = 0;
+function genStateId(): number {
+  stateId += 1;
+  return stateId;
+}
+
+const STATE_ID_PROPERTY = "___testStateStateId";
+
+let initialStates: Map<number, any> = new Map();
+
+export function resetState<T extends object>(state: T): void {
+  if (STATE_ID_PROPERTY in state) {
+    const stateId = (state as any)[STATE_ID_PROPERTY];
+    const initState = initialStates.get(stateId);
+
+    for (const key in state) {
+      state[key] = initState[key];
+    }
+
+    return;
+  }
+
+  const stateId = genStateId();
+  initialStates.set(stateId, JSON.parse(JSON.stringify(state)));
+  Object.defineProperty(state, STATE_ID_PROPERTY, {
+    enumerable: false,
+    writable: false,
+    configurable: false,
+    value: stateId,
+  });
 }
